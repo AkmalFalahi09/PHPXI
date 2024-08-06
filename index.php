a6266dc22739a0e3187c9c387795bd895d0c524d
AkmalFalahi09 <akmalfalahi09@gmail.com>
2024-08-07 06:08:01 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$harga = 12500;
+$jumlah = 4;
+$uang = 50000;
+
+echo "Uang : Rp.".$uang."<br>";
+
+function hitung($harga, $jumlah) {
+    return $harga * $jumlah;
+}
+
+$total = hitung($harga, $jumlah);
+echo "Total : Rp." . $total . "<br>";
+
+if ($total == $uang) {
+    echo "Uangnya pas ya mas";
+} 
+else if ($total < $uang) {
+    $kembalian = $uang - $total;
+    echo "Kembalian Rp." . $kembalian ;
+} 
+else {
+    $kurang = $total - $uang;
+    echo "Kurang Uang : Rp.".$kurang;
+}
+?>
